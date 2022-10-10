24d6318635a7a129eb743fb7b89d94d65de25e2b
Edgar Rivera <edgar.riveraj@gmail.com>
2022-10-10 17:31:36 -0500

    quitando data

--- SearchFilter/src/data/persons.js
+++
@@ -1,4 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(() => console.log('Algo salio mal en el fetch'))
